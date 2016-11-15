4d5d361e818f642e96b4ea8f42abfde64dcb7c32
205081w <205081w@gs.kochi-tech.ac.jp>
2016-11-15 11:49:38 +0900

    icpc-PA.c

---
+++ icpc-PA.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, n;
+    int score[1000];
+    int diffmin;
+
+    while (1) {
+        scanf("%d", &n);
+        if (n == 0) break;
+
+        for (i = 0; i < n; i++) {
+            scanf("%d", &score[i]);
+        }
+
+        diffmin = 1000000; 
+        for (i = 0; i < n; i++) {
+            for (j = n-1; j > i; j--) {
+                if (score[i] - score[j] > 0) {
+                    if (diffmin > (score[i] - score[j])) {
+                        diffmin = score[i] - score[j];
+                    }
+                } else {
+                    if (diffmin > (-1)*(score[i] - score[j])) {
+                        diffmin = (-1)*(score[i] - score[j]);
+                    }
+
+                }
+            }
+        }
+
+        printf("%d\n", diffmin);
+    }
+
+    return 0;
+}
+
