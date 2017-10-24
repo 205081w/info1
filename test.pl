9f8d3a03875c742453aef70fa41b7f4f8f01904a
205081w <shibuta@mulan.iwatalab>
2017-10-24 12:53:49 +0900

    modified Makefile

--- test.pl
+++ test.pl
@@ -5,16 +5,19 @@ foreach(readdir(DIRHANDLE)) {
     next if /^\.{1,2}$/;
     next if /Makefile/;
     next if /test.pl/;
+    next if /test.sh/;
     next if /2017/;
 
     $name = $_;
     if ($name =~ s/\((.*?)\)/$1/) {
         $name = $1;
     }
+    $name =~ s/[\s ]+//g;
     #print "$name\n";
 
     rename($_, $name."_file");
-    #system("cp Makefile_tmp ".$name);
+    system("cp Makefile_tmp ".$name."_file/Makefile");
+    system("cp test.sh ".$name."_file/test.sh");
 }
 
 closedir(DIRHANDLE);
