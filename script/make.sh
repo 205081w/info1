9a9fb73982b2e764ed6b6f63085bc93788e9d10d
205081w <205081w@gs.kochi-tech.ac.jp>
2017-10-26 17:40:53 +0900

    modified Makefile and make.sh

--- script/make.sh
+++ script/make.sh
@@ -2,11 +2,16 @@
 
 SUBDIRS=`find . -type d -name "*_dir"`
 
+echo -n >> test.out
+
 for DIRS in ${SUBDIRS}; do
     echo -e "\n-- ${DIRS} --"
     cd $DIRS
     MAKE=`./test.sh`
     echo $MAKE
+    echo "--- $DIRS ---" >> ../test.out
+    cat test.out >> ../test.out
     cd ..
 done
+
 
